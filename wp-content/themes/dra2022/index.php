f15fd5f89f6833fc09aced1bcab5888992e08fd0
9ero <82823629+9ero@users.noreply.github.com>
2022-04-26 02:19:46 -0600

    test4

--- wp-content/themes/dra2022/index.php
+++ wp-content/themes/dra2022/index.php
@@ -3,6 +3,7 @@
 <!-- content -->
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab molestiae repellendus illum consequatur atque cumque ea eos voluptates minus ipsum! Consequuntur quam esse fuga eius nostrum ullam in quaerat provident!</p>
 <p>asdas</p>
+<h1>agg tmr</h1>
 <!-- content -->  
 
 <?php get_footer(); ?>
