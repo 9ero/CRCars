bb95c35dde92e39bec4291b9cd1936473a51d6fb
9ero <82823629+9ero@users.noreply.github.com>
2022-04-26 02:16:33 -0600

    test3

--- wp-content/themes/dra2022/index.php
+++ wp-content/themes/dra2022/index.php
@@ -2,7 +2,7 @@
 
 <!-- content -->
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab molestiae repellendus illum consequatur atque cumque ea eos voluptates minus ipsum! Consequuntur quam esse fuga eius nostrum ullam in quaerat provident!</p>
-<!-- content -->
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, iure iusto molestias aliquam numquam non praesentium minima repellendus quaerat, similique, libero ut magnam distinctio magni expedita voluptates est. Eveniet, eos!</p>  
-<p>esto es una prueba 2 + 2</p>
+<p>asdas</p>
+<!-- content -->  
+
 <?php get_footer(); ?>
