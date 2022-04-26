d417f541c9538fdc84e69d31b2dcc95cafb87b52
9ero <82823629+9ero@users.noreply.github.com>
2022-04-26 01:19:50 -0600

    Prueba en pantheon
    test de hqdeploy

---
+++ wp-content/themes/dra2022/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<!-- content -->
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab molestiae repellendus illum consequatur atque cumque ea eos voluptates minus ipsum! Consequuntur quam esse fuga eius nostrum ullam in quaerat provident!</p>
+<!-- content -->
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, iure iusto molestias aliquam numquam non praesentium minima repellendus quaerat, similique, libero ut magnam distinctio magni expedita voluptates est. Eveniet, eos!</p>  
+<p>esto es una prueba</p>
+<?php get_footer(); ?>
